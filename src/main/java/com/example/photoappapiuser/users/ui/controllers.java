3179f0efb1d9215132e6731fc6fc34cf84d2265f
stevedeey <stvolutosin69@gmail.com>
2021-06-14 23:04:25 +0100

    create user microservice

---
+++ src/main/java/com/example/photoappapiuser/users/ui/controllers.java
@@ -0,0 +1,16 @@
+package com.example.photoappapiuser.users.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class controllers {
+
+    @GetMapping("/status/check")
+    public String status(){
+        return "Working..";
+    }
+
+}
